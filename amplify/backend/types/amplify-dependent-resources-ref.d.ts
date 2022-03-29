0f5f1af90c915c3f758c62f302e72b2ccd226e73
Catherine Elliott <catherine.elliott@leeds.gov.uk>
2022-03-29 11:27:00 +0100

    added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "ceimagegallery5ba2f6e0": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React from 'react';
 import logo from './celogo.PNG';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>We now have Auth!</h1>
+      </header>
+      <div>
         <p>
           New site coming soon.
         </p>
@@ -17,9 +22,10 @@ function App() {
         >
           Buy stuff here
         </a>
-      </header>
+      </div>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
